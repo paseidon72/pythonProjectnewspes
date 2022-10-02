18fdf055980e9e94c9964a3c7b2efc598b90bf21
paseidon72 <113462667+paseidon72@users.noreply.github.com>
2022-10-02 22:24:35 +0300

    new fail

--- consol.py
+++ consol.py
@@ -1,9 +1,3 @@
 
-A = int(input())
-B = int(input())
-C = int(input())
-D = int(input())
-cost1 = A * 100 + B
-cost2 = C * 100 + D
-total = cost1 + cost2
-print(total // 100, 'грн', total % 100, 'коп')
+n = int(input())
+print((1 - n) // 1)
--- test_1/lesson_2.py
+++ test_1/lesson_2.py
@@ -1,8 +1,4 @@
 
-n = int(input('enter n: '))
-dig1 = n // 100
-n = n - dig1 * 100
-dig2 = n // 10
-dig3 = n - dig2 * 10
-print(dig1 + dig2 + dig3)
+n = int(input())
+print((1 - n) // 1)
 
