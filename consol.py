1df62dcc8a9f6ca02d0e9cdb5727763417e57ed3
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-10-04 10:52:45 +0300

    new function test

--- consol.py
+++ consol.py
@@ -1,3 +1,9 @@
 
-n = int(input())
-print((n // 10) % 10)
+a = int(input('enter first number: '))
+b = int(input('enter second number: '))
+k = (a % b) // a * b
+l = (b % a) // b * a
+n = (a // b) * (b // a) *a
+print(k + l + n)
+
+
