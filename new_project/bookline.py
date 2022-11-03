d467e867ca736336c958180a995d9653d98a4c32
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-03 17:21:22 +0200

    new function test

--- new_project/bookline.py
+++ new_project/bookline.py
@@ -11,7 +11,7 @@ app = Dash(__name__)
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [14, 1, 2, 2, 4, 5],
+    "Amount": [14, 10, 12, 22, 24, 15],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
