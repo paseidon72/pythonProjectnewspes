f47245def8d4c2e71be99db936871079920dd1c5
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-11 21:46:08 +0200

    tablexcel

---
+++ new_project/tablexcel/tablici_2.py
@@ -0,0 +1,20 @@
+import  os
+
+import openpyxl as ox
+import pandas as pd
+
+def update_spreadsheet(path: str, _df, starcol: int = 1, startrow: int = 1, sheet_name: str = "ToUpdate"):
+    '''
+
+    :param path: Путь до файла Excel
+    :param _df: Датафрейм Pandas для записи
+    :param starcol: Стартовая колонка в таблице листа Excel, куда буду писать данные
+    :param startrow: Стартовая строка в таблице листа Excel, куда буду писать данные
+    :param sheet_name: Имя листа в таблице Excel, куда буду писать данные
+    :return:
+    '''
+    wb = ox.load_workbook(path)
+    for ir in range(0, len(_df)):
+        for ic in range(0, len(_df.iloc[ir])):
+            wb[sheet_name].cell(startrow + ir, starcol + ic).value = _df.iloc[ir][ic]
+    wb.save(path)
