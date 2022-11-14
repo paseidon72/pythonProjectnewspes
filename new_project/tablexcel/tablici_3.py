ebedf8900a71e34df3736cfe793884b7985ff521
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-14 21:20:39 +0200

    tablexcel

--- new_project/tablexcel/tablici_3.py
+++ new_project/tablexcel/tablici_3.py
@@ -3,14 +3,16 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import pandas as pd
 
-df = pd.read_excel("excel-comp-data.xlsx")
-df["total"] = df["Jan"] + df["Feb"] + df["Mar"]
-df["Jan"].sum(), df["Jan"].mean(), df["Jan"].min(), df["Jan"].max()
-sum_row = df[["Jan", "Feb", "Mar", "total"]].sum()
-df_sum = pd.DataFrame(data=sum_row).T
-df_sum = df_sum.reindex(columns=df.columns)
-df_final = df.append(df_sum, ignore_index=True)
-df_final.tail(3)
+df = pd.read_excel("excel-comp-data.xlsx") #открываем таблицу во фрейме
+df["total"] = df["Jan"] + df["Feb"] + df["Mar"] # добавить столбец с итогами, чтобы показать общие продажи
+# за январь, февраль и март.
+df["Jan"].sum(), df["Jan"].mean(), df["Jan"].min(), df["Jan"].max() #щоб отримати підсумки за місяць.
+sum_row = df[["Jan", "Feb", "Mar", "total"]].sum() #створіть суму для стовпців місяця та підсумку.
+df_sum = pd.DataFrame(data=sum_row).T #якщо ви хочете додати підсумки у вигляді рядка.
+# Ця T функція дозволяє нам переключати дані з рядків на стовпці.
+df_sum = df_sum.reindex(columns=df.columns) #додати відсутні стовпці
+df_final = df.append(df_sum, ignore_index=True)  #Тепер відформатований DataFrame,
+df_final.tail(3)  # ми можемо додати його до існуючого за допомогою append .
 df.head()
 state_to_code = {"VERMONT": "VT", "GEORGIA": "GA", "IOWA": "IA", "Armed Forces Pacific": "AP", "GUAM": "GU",
                  "KANSAS": "KS", "FLORIDA": "FL", "AMERICAN SAMOA": "AS", "NORTH CAROLINA": "NC", "HAWAII": "HI",
@@ -28,34 +30,40 @@ state_to_code = {"VERMONT": "VT", "GEORGIA": "GA", "IOWA": "IA", "Armed Forces P
                  "NORTH DAKOTA": "ND", "Armed Forces Europe": "AE", "PENNSYLVANIA": "PA", "OKLAHOMA": "OK",
                  "KENTUCKY": "KY", "RHODE ISLAND": "RI", "DISTRICT OF COLUMBIA": "DC", "ARKANSAS": "AR",
                  "MISSOURI": "MO", "TEXAS": "TX", "MAINE": "ME"}
-
-
+#фрагмент кода, нам нужен, отображение имени штата в аббревиатуру.
+#функция сопоставления нечеткого текста:
+process.extractOne("Minnesotta", choices=state_to_code.keys())
+process.extractOne("AlaBAMMazzz", choices=state_to_code.keys(), score_cutoff=80)
 def convert_state(row):
     if pd.notnull(row['state']):
         abbrev = process.extractOne(row["state"], choices=state_to_code.keys(), score_cutoff=80)
         if abbrev:
             return state_to_code[abbrev[0]]
     return np.NaN
+#В функции мы либо возвращаем допустимое сокращение, либо np.NaN, чтобы у нас были допустимые значения в поле.
 
-
-df_final.insert(6, "abbrev", np.NaN)
+df_final.insert(6, "abbrev", np.NaN) #Добавьте столбец в нужном месте и заполните его значениями NaN:
 df_final.head()
-df_final['abbrev'] = df_final.apply(convert_state, axis=1)
+df_final['abbrev'] = df_final.apply(convert_state, axis=1) #Теперь используем apply для добавления сокращений
+# в столбец abbrev:
 df_final.tail()
-df_sub = df_final[["abbrev", "Jan", "Feb", "Mar", "total"]].groupby('abbrev').sum()
+df_sub = df_final[["abbrev", "Jan", "Feb", "Mar", "total"]].groupby('abbrev').sum() #Создание промежуточного итога
+# в pandas выполняется с помощью метода groupby:
 
 
 
-def money(x):
+def money(x): #Затем хотим отобразить данные с обозначением валюты, используя applymap для
+    # всех значений в кадре данных:
     return "${:,.0f}".format(x)
 
 
 formatted_df = df_sub.applymap(money)
-sum_row = df_sub[["Jan", "Feb", "Mar", "total"]].sum()
-df_sub_sum = pd.DataFrame(data=sum_row).T
+sum_row = df_sub[["Jan", "Feb", "Mar", "total"]].sum() #теперь можем получить итоговые значения, как раньше:
+df_sub_sum = pd.DataFrame(data=sum_row).T #Преобразуйте значения в столбцы и отформатируйте их:
 df_sub_sum = df_sub_sum.applymap(money)
-final_table = formatted_df.append(df_sub_sum)
-final_table = final_table.rename(index={0: "Total"})
+final_table = formatted_df.append(df_sub_sum) #добавьте итоговое значение в DataFrame:
+final_table = final_table.rename(index={0: "Total"}) #Вы заметите, что для итоговой строки индекс равен 0.
+#Можем изменить это с помощью метода rename:
 
 # print(df.head())
 # print('*' * 50)
